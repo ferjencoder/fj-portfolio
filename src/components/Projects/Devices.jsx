e68ac595506befa9e04468b9284967f20e8c081c
ferJen <ferjen.coder@gmail.com>
2023-06-17 23:21:07 -0300

    chore: Touch ups in Projects

--- src/components/Projects/Devices.jsx
+++ src/components/Projects/Devices.jsx
@@ -9,11 +9,13 @@ export const Devices = () => {
     const [ rotationDegreeLow, setRotationDegreeLow ] = useState( 0 );
 
     const projects = [
-        { name: 'Project 1', gif: 'https://res.cloudinary.com/ferjen/image/upload/v1674531275/portfolio/projects/gif/VeggieBox_hk61er.gif' },
-        { name: 'Project 2', gif: 'https://res.cloudinary.com/ferjen/image/upload/v1686533830/portfolio/video/static-low_sanazm.gif' },
-        { name: 'Project 2', gif: 'https://res.cloudinary.com/ferjen/image/upload/v1686533830/portfolio/video/static-low_sanazm.gif' },
-        // { name: 'Project 3', gif: 'https://res.cloudinary.com/ferjen/video/upload/v1682158301/portfolio/video/ferJen_iyvdi3.mp4' },
-        // Add more projects here...
+        { name: 'Project 1', gif: 'https://res.cloudinary.com/ferjen/image/upload/v1686533830/portfolio/video/static-low_sanazm.gif' },
+        { name: 'Project 2', gif: 'https://res.cloudinary.com/ferjen/image/upload/v1674531275/portfolio/projects/gif/VeggieBox_hk61er.gif' },
+        { name: 'Project 3', gif: 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeDU3b3FpNGM5ZHB0cDQ3aDFhamoxMDZwMG95b2ZkeDkwOGc2eHMxeCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/470MxBwLQsgwRFO6bx/giphy.gif' },
+        { name: 'Project 4', gif: 'https://media.giphy.com/media/hVrYqWSpU8GPHreQX0/giphy.gif' },
+        { name: 'Project 5', gif: 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdnVlM3BkMWQzd2p4Y2o5c3hiNXE0a3l0ZGFzNjN0M2hwc3BxdzZ3bCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/vWTs1YICbpa97bEbyp/giphy.gif' }
+
+        // <div style="width:100%;height:0;padding-bottom:56%;position:relative;"><iframe src="https://giphy.com/embed/vWTs1YICbpa97bEbyp" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/vWTs1YICbpa97bEbyp">via GIPHY</a></p>
     ];
 
     const nextProject = () => {
@@ -32,13 +34,12 @@ export const Devices = () => {
                 <div className='laptop__container'>
                     <img
                         className='laptop__image'
-                        src="https://res.cloudinary.com/ferjen/image/upload/v1687033742/portfolio/projects/devices/tv-violet-space_oqdm63.png"
+                        src="https://res.cloudinary.com/ferjen/image/upload/v1687048724/portfolio/projects/devices/DreamShaper-TV-Space-removebg_cyehfy.png"
                         alt="" />
                     <img
                         className='veggieBox__video'
                         src={projects[ currentProject ].gif}
                         alt="" />
-                    {/* <video className='veggieBox__video' src="https://res.cloudinary.com/ferjen/video/upload/v1686544154/portfolio/video/FJ-Intro_vk7pt1.mp4" autoPlay></video> */}
                     <div className='video-overlay'></div>
                     <div className='tv-btn__container'>
                         <button
@@ -52,10 +53,6 @@ export const Devices = () => {
                                         className='tv-btn__image'
                                         src="https://res.cloudinary.com/ferjen/image/upload/v1687042392/portfolio/projects/devices/tv-knob_oyd2wf.png" alt="" />
                                 </div>
-                                {/* <div className='dot dot-1'></div>
-                                <div className='dot dot-2'></div>
-                                <div className='dot dot-3'></div>
-                                <div className='dot dot-4'></div> */}
                             </div>
                         </button>
 
@@ -65,22 +62,8 @@ export const Devices = () => {
                             style={{ transform: `rotate(${rotationDegreeLow}deg)` }}
                         >
                             MORE INFO
-                            {/* <img
-                            className='btn-next--btn'
-                            src="https://res.cloudinary.com/ferjen/image/upload/v1686529555/portfolio/projects/devices/TV-btn_rb1rww.png"
-                            alt="" /> */}
                         </button>
 
-                        {/* <button
-                        className='btn-next btn-next--low'
-                        onClick={prevProject}
-                        style={{ transform: `rotate(${rotationDegreeLow}deg)` }}
-                    >
-                        <img
-                            className='btn-next--btn'
-                            src="https://res.cloudinary.com/ferjen/image/upload/v1686529555/portfolio/projects/devices/TV-btn_rb1rww.png"
-                            alt="" />
-                    </button> */}
                     </div>
                 </div>
             </div>
